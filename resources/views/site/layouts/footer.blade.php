d1fc47907c347f11556ff16f49963d65c8a7c7a8
Gabriel Loureiro Fortes Guimaraes <gabrielglfg@outlook.com>
2022-03-29 17:59:10 -0300

    footer finished

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -1,43 +1,67 @@
 <section class="footer-bg">
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 d-flex justify-content-between">
-                <ul>
-                    <li class="footer-nav__li">
-                        <a class="text-dark"href="">
-                            Sobre nós
-                        </a>
-                    </li>
-                    <li class="footer-nav__li">
-                        <a class="text-dark"href="">
-                            Fórmula
-                        </a>
-                    </li>
-                    <li class="footer-nav__li">
-                        <a class="text-dark"href="">
-                            Benifícios
-                        </a>
-                    </li>
-                </ul>
-                <ul>
-                    <li class="footer-nav__li">
-                        <a class="text-dark"href="">
-                            Compre já
-                        </a>
-                    </li>
-                    <li class="footer-nav__li">
-                        <a class="text-dark"href="">
-                            FAQ
-                        </a>
-                    </li>
-                </ul>
-                <div class="d-flex flex-column" style="width: 50%">
+            <div class="col-lg-6">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <ul>
+                            <li class="footer-nav__li">
+                                <a class="text-dark"href="">
+                                    Sobre nós
+                                </a>
+                            </li>
+                            <li class="footer-nav__li">
+                                <a class="text-dark"href="">
+                                    Fórmula
+                                </a>
+                            </li>
+                            <li class="footer-nav__li">
+                                <a class="text-dark"href="">
+                                    Benifícios
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2">
+                        <ul>
+                            <li class="footer-nav__li">
+                                <a class="text-dark"href="">
+                                    Compre já
+                                </a>
+                            </li>
+                            <li class="footer-nav__li">
+                                <a class="text-dark"href="">
+                                    FAQ
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                          
+                <div class="d-flex flex-column">
                     <p class="text-dark text-uppercase text-center">Formas de pagamento</p>
                     <div>
                         <img class="img-responsive" src={{asset('assets/site/img/cards.svg')}} alt="">
                     </div>
                 </div>
+                    </div>
+                </div>
+               
              
+             
+            </div>
+            <div class="col-lg-6 d-flex align-items-center justify-content-center">
+                <div>
+                <img src={{asset('assets/site/img/seal-footer.svg')}} alt="">
+                </div>
+                <div class="ms-3">
+                    <p class="text-center text-dark mb-2 text-uppercase"> 
+                        Acompanhe seu pedido
+                    </p>
+                    <a class="btn-buy d-flex align-items-center justify-content-between p-3" href="https://envios.greenn.com.br/" target="_blank">
+                        Rastrear <img src={{asset('assets/site/img/truck.svg')}} alt="">
+                    </a>
+                </div>
             </div>
         </div>
     </div>
