fc55b7726e481a42eab92165d4eab2d5a4758899
Gabriel Loureiro Fortes Guimaraes <gabrielglfg@outlook.com>
2022-03-29 15:23:09 -0300

    banner w/o responsive

--- resources/views/site/layouts/banner.blade.php
+++ resources/views/site/layouts/banner.blade.php
@@ -1,8 +1,32 @@
 <section class="p-0">
     <ul class="rslides">
-        <li><img src={{asset('assets/site/img/slide1.png')}} alt=""></li>
-        <li><img src={{asset('assets/site/img/slide2.png')}} alt=""></li>
-        <li><img src={{asset('assets/site/img/slide3.png')}} alt=""></li>
-      
-      </ul>
+        <li style="position: relative">
+          <img src={{ asset('assets/site/img/slide1.png') }} alt="">
+          <div class="row banner-btn__row top">
+            <div class="col-lg-12 banner-btn__col">
+                <a href="" class="btn-buy">quero comprar agora</a>
+            </div>
+        </div>
+          <div class="row banner-btn__row first">
+            <div class="col-lg-12 banner-btn__col">
+                <a href="" class="btn-buy">quero comprar agora</a>
+            </div>
+        </div>
+      </li>
+        <li><img src={{ asset('assets/site/img/slide2.png') }} alt="">
+            <div class="row justify-content-center" style="position:relative; bottom: 80px;">
+                <div class="col-lg-3 ms-5">
+                    <a href="" class="btn-buy">quero comprar agora</a>
+                </div>
+            </div>
+        </li>
+        <li style="position: relative;"><img src={{ asset('assets/site/img/slide3.png') }} alt="">
+            <div class="row banner-btn__row third">
+                <div class="col-lg-12 banner-btn__col">
+                    <a href="" class="btn-buy">quero comprar agora</a>
+                </div>
+            </div>
+        </li>
+
+    </ul>
 </section>
