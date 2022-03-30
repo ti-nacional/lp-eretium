bfd252d02e6a6133078100ab885caa4f3c2e030d
Gabriel Loureiro Fortes Guimaraes <gabrielglfg@outlook.com>
2022-03-30 09:59:40 -0300

    banner changed and responsive adjusts

--- resources/views/site/layouts/banner.blade.php
+++ resources/views/site/layouts/banner.blade.php
@@ -1,32 +1,11 @@
 <section class="p-0">
-    <ul class="rslides">
-        <li style="position: relative">
-          <img src={{ asset('assets/site/img/slide1.png') }} alt="">
-          <div class="row banner-btn__row top">
-            <div class="col-lg-12 banner-btn__col">
-                <a href="" class="btn-buy">quero comprar agora</a>
-            </div>
-        </div>
-          <div class="row banner-btn__row first">
-            <div class="col-lg-12 banner-btn__col">
-                <a href="" class="btn-buy">quero comprar agora</a>
-            </div>
-        </div>
-      </li>
-        <li><img src={{ asset('assets/site/img/slide2.png') }} alt="">
-            <div class="row justify-content-center" style="position:relative; bottom: 80px;">
-                <div class="col-lg-3 ms-5">
-                    <a href="" class="btn-buy">quero comprar agora</a>
-                </div>
-            </div>
-        </li>
-        <li style="position: relative;"><img src={{ asset('assets/site/img/slide3.png') }} alt="">
-            <div class="row banner-btn__row third">
-                <div class="col-lg-12 banner-btn__col">
-                    <a href="" class="btn-buy">quero comprar agora</a>
-                </div>
-            </div>
-        </li>
+<img class="img-responsive d-sm-none d-md-block" src={{asset('assets/site/img/slide2.png')}} alt="">
+<img class="img-responsive d-sm-block d-md-none" src={{asset('assets/site/img/banner.png')}} alt="">
 
-    </ul>
+<div class="row justify-content-center" style="position:relative; bottom: 80px; width: 100%">
+    <div class="col-lg-4 d-flex justify-content-center">
+        <a href="" class="btn-buy fw-heavy">quero comprar agora</a>
+    </div>
+</div>
+
 </section>
